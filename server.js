17f6b7965c1cf9f478d76591008f36a7c4d3cdb2
JALZ <jannus.largo@correounivalle.edu.co>
2023-08-28 21:01:15 -0500

    Add database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config.js";
+import "./config/database.js";
 
 const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT;
 
