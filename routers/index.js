e40ac5caebda5f919354db802b107f1e70fec217
JALZ <jannus.largo@correounivalle.edu.co>
2023-09-09 15:05:00 -0500

    Add states

--- routers/index.js
+++ routers/index.js
@@ -3,12 +3,14 @@ import { Router } from "express";
 import productsRouter from "../routers/products.js";
 import categoriesRouter from './categories.js';
 import authRouter from './auth.js';
+import routerStates from "./states.js";
 
 const indexRouter = Router();
 
 indexRouter.use('/products', productsRouter);
 indexRouter.use('/auth', authRouter);
 indexRouter.use('/categories', categoriesRouter);
+indexRouter.use('/states', routerStates);
 
 export default indexRouter;
 
