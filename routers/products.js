79311c943b0ded3f6ea9e6be975342a2352e6517
Santiago Vanegas Mejia <125935085+santivanegas16@users.noreply.github.com>
2023-09-09 10:27:40 -0500

    controllers admi products and user created (#9)

--- routers/products.js
+++ routers/products.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import read from "../controllers/products/read.js";
+import readAdmi from "../controllers/products/readAdmi.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/", read);
+productsRouter.get("/admi", readAdmi);
+
 
 export default productsRouter;
