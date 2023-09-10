4834cd88d149512c8e81086a219fc632d19f8c62
JALZ <jannus.largo@correounivalle.edu.co>
2023-09-09 20:05:15 -0500

    Create read_one and destroy in Products

--- routers/products.js
+++ routers/products.js
@@ -1,11 +1,23 @@
+// bookshop
 import { Router } from "express";
+
+// Controllers
 import read from "../controllers/products/read.js";
 import readAdmi from "../controllers/products/readAdmi.js";
+import read_one from "../controllers/products/read_one.js";
+import destroy from "../controllers/products/destroy.js";
 
+// Middlewares
+import passport from '../middlewares/passport.js';
+import is_creator_or_admin from "../middlewares/is_creator_or_admin.js";
+
+// Router
 const productsRouter = Router();
 
 productsRouter.get("/", read);
 productsRouter.get("/admi", readAdmi);
+productsRouter.post('/:id', read_one);
+productsRouter.post('/destroy/:id', passport.authenticate('jwt', { session: false }), is_creator_or_admin, destroy);
 
 
 export default productsRouter;
