e13010d92f36e5662e5be44722b1ee0030ffca11
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-09-09 20:52:38 -0500

    Controllers products (#14)
    * Create read_one and destroy in Products
    
    * Create update in Products
    
    * Create endpoint in products for create new product

--- controllers/products/read.js
+++ controllers/products/read.js
@@ -3,21 +3,13 @@ import Product from "../../Models/Product.js";
 export default async (req, res, next) => {
 	try {
 		const search = {};
-		const pagination = { page: 1, limit: 6 };
+		const pagination = { page: 1, limit: 8 };
 		const ordering = { price: 1 };
 
-		if (req.query.name) {
-			search.name = new RegExp(req.query.name.trim(), "i");
-		}
-		if (req.query.category) {
-			search.category_id = req.query.category.split(",");
-		}
-		if (req.query.page) {
-			pagination.page = Number(req.query.page);
-		}
-		if (req.query.quantity) {
-			pagination.limit = Number(req.query.quantity);
-		}
+		if (req.query.name) { search.name = new RegExp(req.query.name.trim(), "i"); }
+		if (req.query.category) { search.category_id = req.query.category.split(","); }
+		if (req.query.page) { pagination.page = Number(req.query.page); }
+		if (req.query.quantity) { pagination.limit = Number(req.query.quantity); }
 
 		const skip = (pagination.page - 1) * pagination.limit;
 		const limit = pagination.limit;
@@ -30,18 +22,9 @@ export default async (req, res, next) => {
 		const all = await Product.find(search).skip(skip).limit(limit).sort(ordering);
 
 		if (all.length > 0) {
-			return res.status(200).json({
-				response: all,
-				message: "Products found!",
-				prev,
-				next,
-				pages,
-			});
+			return res.status(200).json({ success: true, response: all, message: "Products found!", prev, next, pages });
 		} else {
-			return res.status(400).json({
-				response: null,
-				message: "Products not found!",
-			});
+			return res.status(400).json({ success: false, response: null, message: "Products not found!" });
 		}
 	} catch (error) {
 		next(error);
--- routers/products.js
+++ routers/products.js
@@ -1,11 +1,31 @@
+// bookshop
 import { Router } from "express";
+
+// Controllers
 import read from "../controllers/products/read.js";
 import readAdmi from "../controllers/products/readAdmi.js";
+import read_one from "../controllers/products/read_one.js";
+import destroy from "../controllers/products/destroy.js";
+import update from "../controllers/products/update.js";
 
+// Middlewares
+import passport from '../middlewares/passport.js';
+import is_creator_or_admin from "../middlewares/is_creator_or_admin.js";
+import validator from '../middlewares/validator.js';
+import add_creator_in_product from "../middlewares/add_creator_in_product.js";
+
+// Schema
+import schemaProduct from '../schemas/products/create.js';
+import create from "../controllers/products/create.js";
+
+// Router
 const productsRouter = Router();
 
 productsRouter.get("/", read);
 productsRouter.get("/admi", readAdmi);
-
+productsRouter.post('/', passport.authenticate('jwt', { session: false }), is_creator_or_admin, validator(schemaProduct), add_creator_in_product, create);
+productsRouter.post('/:id', read_one);
+productsRouter.post('/destroy/:id', passport.authenticate('jwt', { session: false }), is_creator_or_admin, destroy);
+productsRouter.post('/update/:id', passport.authenticate('jwt', { session: false }), is_creator_or_admin, update);
 
 export default productsRouter;
