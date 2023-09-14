c3b7cbeb9ed7f24af8b91452595022e315924d70
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-09-14 14:21:51 -0500

    Creators (#30)
    * creators controllers
    
    * query readCreator

--- controllers/products/readCreator.js
+++ controllers/products/readCreator.js
@@ -2,8 +2,10 @@ import Product from "../../Models/Product.js";
 
 export default async (req, res, next) => {
     try {
-        const productsCreator = await Product.find({ creator_id: req.creator._id }, "-createdAt -updatedAt -__v -creator_id")
-            .populate("category_id", "name color");
+        const queries = {};
+        if (req.query.name) { queries.name = new RegExp(req.query.name.trim(), 'i') }
+        const productsCreator = await Product.find({ creator_id: req.creator._id }, "-createdAt -updatedAt -__v -creator_id").populate("category_id", "name color").find(queries)
+            
         if (productsCreator.length > 0) {
             return res.status(200).json({ success: true, response: productsCreator, message: 'Products Found' });
         }
